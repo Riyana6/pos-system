01f95fdfde6be9be5a2350766bda65c24c324d61
Riyana6 <jeemariyana@gmail.com>
2025-07-17 14:02:18 +0530

    new changes

--- pos-frontend/src/components/auth/Login.jsx
+++ pos-frontend/src/components/auth/Login.jsx
@@ -1,82 +1,89 @@
-import { useState } from "react";
-import { login } from "../../https/index";
-import { useMutation } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { useMutation } from "@tanstack/react-query"
+import { login } from "../../https/index"
 import { enqueueSnackbar } from "notistack";
 import { useDispatch } from "react-redux";
 import { setUser } from "../../redux/slices/userSlice";
 import { useNavigate } from "react-router-dom";
-
-export default function Login() {
-    const dispatch = useDispatch();
+ 
+const Login = () => {
     const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-            email: "",
-            password: "",
+    const dispatch = useDispatch();
+    const[formData, setFormData] = useState({
+      email: "",
+      password: "",
     });
-
+  
     const handleChange = (e) => {
-        setFormData({...formData, [e.target.name]: e.target.value});
-    };
+      setFormData({...formData, [e.target.name]: e.target.value});
+    }
 
+  
     const handleSubmit = (e) => {
-        e.preventDefault();
-        loginMutation.mutate(formData);
+      e.preventDefault();
+      loginMutation.mutate(formData);
     }
 
     const loginMutation = useMutation({
-        mutationFn: (reqData) => login(reqData),
-        onSuccess: (res) => {
-            const { data } = res;
-            console.log("Login successful:", data);
-            const {_id, name, email, phone, role } = data.data;
-            dispatch(setUser({_id, name, email, phone, role }));
-            navigate("/");
-            // Handle successful login, e.g., redirect or show success message
-        },
-        onError: (error) => {
-            console.error("Login failed:", error);
-            enqueueSnackbar(response.data.message, { variant: "error" });
-        }
-    });
+      mutationFn: (reqData) => login(reqData),
+      onSuccess: (res) => {
+          const { data } = res;
+          console.log(data);
+          const { _id, name, email, phone, role } = data.data;
+          dispatch(setUser({ _id, name, email, phone, role }));
+          navigate("/");
+      },
+      onError: (error) => {
+        const { response } = error;
+        enqueueSnackbar(response.data.message, { variant: "error" });
+      }
+    })
+
   return (
     <div>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label className="block text-[#ababab] mb-2 mt-3 text-sm font-medium">
-                    Employee Email
-                </label>
-                <div className="flex item-center rounded-lg p-5 px-4 bg-[#1f1f1f]">
-                    <input
-                        type="text"
-                        value={formData.email}
-                        onChange={handleChange}
-                        name="email"
-                        placeholder="Enter employee email"
-                        className="bg-transparent flex-1 text-white focus:outline-none"
-                        required
-                    />
-                </div>
-            </div>
-            <div>
-                <label className="block text-[#ababab] mb-2 text-sm font-medium">
-                    Password
-                </label>
-                <div className="flex item-center rounded-lg p-5 px-4 bg-[#1f1f1f]">
-                    <input
-                        type="text"
-                        value={formData.password}
-                        onChange={handleChange}
-                        name="password"
-                        placeholder="Enter password"
-                        className="bg-transparent flex-1 text-white focus:outline-none"
-                        required
-                    />
-                </div>
-            </div>
-            <button type="submit" className="w-full mt-6 bg-yellow-400 text-lg py-3 text-gray-900 font-bold">
-                Sign In
-            </button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label className="block text-[#ababab] mb-2 mt-3 text-sm font-medium">
+            Employee Email
+          </label>
+          <div className="flex item-center rounded-lg p-5 px-4 bg-[#1f1f1f]">
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Enter employee email"
+              className="bg-transparent flex-1 text-white focus:outline-none"
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <label className="block text-[#ababab] mb-2 mt-3 text-sm font-medium">
+            Password
+          </label>
+          <div className="flex item-center rounded-lg p-5 px-4 bg-[#1f1f1f]">
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Enter password"
+              className="bg-transparent flex-1 text-white focus:outline-none"
+              required
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full rounded-lg mt-6 py-3 text-lg bg-yellow-400 text-gray-900 font-bold"
+        >
+          Sign in
+        </button>
+      </form>
     </div>
   );
-}
+};
+
+export default Login;
--- pos-frontend/src/main.jsx
+++ pos-frontend/src/main.jsx
@@ -1,21 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
-import { SnackbarProvider } from 'notistack'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import { SnackbarProvider } from "notistack";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime : 3000
-    },
-  },
+      staleTime : 30000,
+    }
+  }
 })
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <SnackbarProvider autoHideDuration={3000} maxSnack={3}>
@@ -24,5 +24,5 @@ createRoot(document.getElementById('root')).render(
         </QueryClientProvider>
       </SnackbarProvider>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
